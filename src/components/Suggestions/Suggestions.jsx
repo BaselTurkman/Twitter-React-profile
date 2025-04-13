66631155456ce9d202ebbb0509307ae43612f156
BaselTurkman <Basel.Turkman123@gmail.com>
2025-04-13 23:36:51 +0300

    Remove PeopleToFollow.js

--- src/components/Suggestions/Suggestions.jsx
+++ src/components/Suggestions/Suggestions.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { List, Stack } from "@mui/material";
-import { peopleToFollow } from "../../constants/peopleToFollow";
+import { postCards } from "../../constants/postCards";
 import LikeItem from "./LikeItem";
 import TrendsSection from "./TrendsSection";
 import FooterLinks from "../FooterLinks/FooterLinks";
@@ -13,7 +13,7 @@ export default function Suggestions() {
       <SearchBar />
       <BoxContainer title="You might Like">
         <List>
-          {peopleToFollow.map((item) => (
+          {postCards.map((item) => (
             <LikeItem key={item} item={item} />
           ))}
         </List>
