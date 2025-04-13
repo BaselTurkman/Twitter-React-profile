54962b52c1677c604122b7372fb4fa99e8d203d5
BaselTurkman <Basel.Turkman123@gmail.com>
2025-04-13 23:28:13 +0300

    Add isPremium badge for accounts

--- src/constants/peopleToFollow.js
+++ src/constants/peopleToFollow.js
@@ -6,13 +6,16 @@ export const peopleToFollow = [
   {
     name: "Tailwind CSS",
     logo: tailwind,
+    isPremium: true,
   },
   {
     name: "TypeScript",
     logo: typescript,
+    isPremium: false,
   },
   {
     name: "freeCodeCamp.org",
     logo: freeCodeCamp,
+    isPremium: true,
   },
 ];
--- src/constants/postCards.js
+++ src/constants/postCards.js
@@ -6,6 +6,7 @@ export const postCards = [
   {
     name: "Tailwind CSS",
     logo: tailwind,
+    isPremium: true,
     description: "We're a community of mdlions of people who buildir skills getting new jobs together A public charity.",
     tweetBy: "eabbeyrenn",
     follow: "Node.js and 9 others follow"
@@ -13,12 +14,14 @@ export const postCards = [
   {
     name: "TypeScript",
     logo: typescript,
+    isPremium: false,
     description: "TypeScript is a language for application-scale JavaScript development. It's a typed superset of JavaScript that compiles to plain JavaScript.",
     follow: "Node.js and 5 others follow"
   },
   {
     name: "freeCodeCamp.org",
     logo: freeCodeCamp,
+    isPremium: true,
     description: "The utility-first CSS framework. Rapidly build modern websites, without ever leaving your HTML.",
     follow: "Yazeed Obaid and 3 others follow"
   },
